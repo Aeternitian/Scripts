33eeb611b267e88fedd5f94d7b086d58acb0ead4
Aeternitian <143951125+Aeternitian@users.noreply.github.com>
2023-09-06 15:55:06 +0200

    Update FoodSpawn.lua

--- FoodSpawn.lua
+++ FoodSpawn.lua
@@ -1,11 +1,16 @@
 local userInputService = game:GetService("UserInputService")
-userInputService.InputBegan:Connect (function(input, gameProcessedEvent)
-		if input.userInputType == Enum.UserInputType.Keyboard then
-			if input.KeyCode.V then
-            workspace.ServerStuff.dropAmmo:FireServer("rations", "MRE")
+UserInputService.InputBegan:Connect(function(input, Typing)
+    if Typing then
+        return
+    end
+	if input.KeyCode == Enum.KeyCode.R then
+	        for i=1, 10 do
+            workspace.ServerStuff.initiateblock:FireServer(_R.serverKey, true)
+
+          workspace.ServerStuff.dropAmmo:FireServer("rations", "MRE")
             workspace.ServerStuff.dropAmmo:FireServer("rations", "Bottle")
             workspace.ServerStuff.dropAmmo:FireServer("Ammo", "Long")
               workspace.ServerStuff.dropAmmo:FireServer("Ammo", "Small")
-        end
-    end
+	        end
+	end
 end)
