d48bce70f5952bc7d033dc228a553c9b0e47d8ce
Aeternitian <143951125+Aeternitian@users.noreply.github.com>
2023-09-06 16:06:16 +0200

    Update FoodSpawn.lua

--- FoodSpawn.lua
+++ FoodSpawn.lua
@@ -3,4 +3,3 @@ for i=1, 1 do
             workspace.ServerStuff.dropAmmo:FireServer("rations", "Bottle")
             workspace.ServerStuff.dropAmmo:FireServer("Ammo", "Long")
             workspace.ServerStuff.dropAmmo:FireServer("Ammo", "Small")
-end,
