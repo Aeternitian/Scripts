eb7e3bbfc07d6bddcbf4df4c8c725702b8a5ef56
Aeternitian <143951125+Aeternitian@users.noreply.github.com>
2023-09-06 14:05:20 +0200

    Update ParrySpam.lua

--- ParrySpam.lua
+++ ParrySpam.lua
@@ -1,4 +1,11 @@
-if input.KeyCode == Enum.KeyCode.R then
-   for i=1, 10 do
-         workspace.ServerStuff.initiateblock:FireServer(_G.serverKey, true)
-
+        UserInputService.InputBegan:Connect(function(input, Typing)
+    if Typing then
+        return
+    end
+	if input.KeyCode == Enum.KeyCode.R then
+	        for i=1, 10 do
+            workspace.ServerStuff.initiateblock:FireServer(_G.serverKey, true)
+	        end
+	end
+end)
+})
