9fc663a55b0db6f54463868af03dd9998390815c
Aeternitian <143951125+Aeternitian@users.noreply.github.com>
2023-09-07 18:17:13 +0200

    Update ParrySpam.lua

--- ParrySpam.lua
+++ ParrySpam.lua
@@ -1,5 +1,8 @@
-        if tostring(self) == "initiateblock" and Toggles.AutoParry then
-            if Args[2] ~= nil and Args[2] == false then
-                return
-            end
-        end
+local UserInputService = game:GetService("UserInputService")
+UserInputService.InputBegan:Connect(function(input, KeyCode)
+ if input.KeyCode == Enum.KeyCode.R then
+	    for i=1, 10 do
+            workspace.ServerStuff.initiateblock:FireServer(_G.serverKey, true)
+	    end
+	end
+end)
