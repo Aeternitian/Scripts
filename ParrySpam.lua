ba7660e8b15093c00ec5bc0952b665e2b25afec6
Aeternitian <143951125+Aeternitian@users.noreply.github.com>
2023-09-07 18:19:29 +0200

    Update ParrySpam.lua

--- ParrySpam.lua
+++ ParrySpam.lua
@@ -2,7 +2,9 @@ local UserInputService = game:GetService("UserInputService")
 UserInputService.InputBegan:Connect(function(input, KeyCode)
  if input.KeyCode == Enum.KeyCode.R then
 	    for i=1, 10 do
+	    wait()		
             workspace.ServerStuff.initiateblock:FireServer(_G.serverKey, true)
+	    wait()
 	    end
 	end
 end)
